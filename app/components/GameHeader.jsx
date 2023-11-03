79dec3ed65a9dcc3a35d55d5bd7aa704bddbeccb
Brandon Aldred <brandonaldred@gmail.com>
2023-11-03 07:19:07 -0700

    adding link to header

--- app/components/GameHeader.jsx
+++ app/components/GameHeader.jsx
@@ -1,10 +1,11 @@
 import logo from '../assets/images/logo.svg'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function GameHeader() {
     return(
         <header className={styles['black-header']}>
-            <img src={logo.src} alt="Marketing Darts" />
+            <Link href="/"><img src={logo.src} alt="Marketing Darts" /></Link>
         </header>
     )
 }
