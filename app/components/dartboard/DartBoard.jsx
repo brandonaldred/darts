0a1b53ffd40aeca35b7934dda52a51a8b1e05189
Brandon Aldred <brandonaldred@gmail.com>
2023-11-02 17:59:26 -0700

    Update DartBoard.jsx

--- app/components/dartboard/DartBoard.jsx
+++ app/components/dartboard/DartBoard.jsx
@@ -4,9 +4,9 @@ export default function DartBoard(props) {
    
     return(
         <>
-            <p className={styles[props.class]} data-amount={props.content}>{props.content}</p>
-            <p data-amount={props.content * 2}>D</p>
-            <p data-amount={props.content * 3}>T</p>
+            <p className={styles[props.class]} data-amount={props.content} onClick={()=>{props.addDart(props.content)}}>{props.content}</p>
+            <p data-amount={props.content * 2} onClick={()=>{props.addDart(props.content * 2)}}>D</p>
+            <p data-amount={props.content * 3} onClick={()=>{props.addDart(props.content * 3)}}>T</p>
         </>
     )
 }
