8b5e142242971f62085e7a776c329a0dfbec3788
Brandon Aldred <brandonaldred@gmail.com>
2023-11-02 18:20:05 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,8 @@ export default function Home() {
         <div className={styles['game-select-container']}>
           <h2>Select Game Type</h2>
           <div className={styles.games}>
-            <Link className={styles['game-type']} href="#">301</Link>
-            <Link className={styles['game-type']} href="#">501</Link>
+            <Link className={styles['game-type']} href="/01/301">301</Link>
+            <Link className={styles['game-type']} href="/01/501">501</Link>
             <Link className={`${styles['game-type']} ${styles['game-full-width']}`} href="#">Around The World</Link>
             <Link className={`${styles['game-type']} ${styles['game-full-width']}`} href="#">3 Dart High</Link>
             <Link className={`${styles['game-type']} ${styles['game-full-width']}`} href="#">Cricket</Link>
