54e8fc4043b2be3136b59a130b30c20495e49030
Brandon Aldred <brandonaldred@gmail.com>
2023-11-08 05:39:39 -0800

    updating imports

--- app/api/users/create/route.js
+++ app/api/users/create/route.js
@@ -1,5 +1,5 @@
 import connectMongoDB from "@/libs/mongodb";
-import User from "../../../models/User"
+import User from "../../../../models/User"
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt"
 
