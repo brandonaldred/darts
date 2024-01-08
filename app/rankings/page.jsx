b757ab3f6017ea9e4a96f61a89032fd54b6ac091
Brandon Aldred <brandonaldred@gmail.com>
2024-01-08 14:11:58 -0800

    changing some things

--- app/rankings/page.jsx
+++ app/rankings/page.jsx
@@ -3,31 +3,31 @@ import GameHeader from '../components/GameHeader';
 import PlayerCard from '../components/playercard/Playercard';
 import styles from './page.module.css';
 
-export default async function Rankings() {
+async function fetchRankings() {
     if (!process.env.NEXT_PUBLIC_BASE_API_URL) { return null }
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/users/ranking?t=301`)
-    .then(res => res.json())
-    .then(data => {
-       return data.players.map((player, index) => {
-            console.log(player.rank['301'])
-            return (
-                <PlayerCard
-                    key={index}
-                    position={index}
-                    firstName={player.firstName}
-                    username={player.username}
-                    rank={player.rank['301']}
-                />
-                )
-        })
-    });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/users/ranking?t=301`);
+    return res.json()
+    }  
 
+export default async function Rankings() {
+    const res = await fetchRankings()
+    const rankRatings = res.players.map((player, index) => {
+        return (
+            <PlayerCard
+                key={index}
+                position={index}
+                firstName={player.firstName}
+                username={player.username}
+                rank={player.rank['301']}
+            />
+            )
+    })
     return (
         <>
             <GameHeader />
             <div className={styles['content-wrapper']}>
             <h1>Player Rankings</h1>
-                {res}
+                {rankRatings}
             </div>
         </>
     );
